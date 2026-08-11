d54cdcf93bb435ae5056a2d26515b693c5ce09a7
MANISH KUMAR GUPTA <manish.gupta.mech@gmail.com>
2026-08-11 14:11:29 +0530

    update ecossytem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,25 +2,33 @@ module.exports = {
   apps: [
     {
       name: "chat-app",
+
       script: "node_modules/next/dist/bin/next",
-      args: "start",
-      cwd: __dirname,
+      args: "start -p 3000",
+
+      cwd: "/home/dzinlynxt-luna/htdocs/luna.dzinlynxt.com/my_design_pro_chat",
+
       instances: 1,
       exec_mode: "fork",
+
       autorestart: true,
       watch: false,
+
       max_memory_restart: "1G",
+
       env: {
         NODE_ENV: "production",
-        PORT: 3000
+        PORT: 5178
       },
-      env_production: {
-        NODE_ENV: "production"
-      },
-      error_file: "~/logs/chat-app/err.log",
-      out_file: "~/logs/chat-app/out.log",
+
+      error_file: "/home/dzinlynxt-luna/logs/chat-app/err.log",
+      out_file: "/home/dzinlynxt-luna/logs/chat-app/out.log",
+
       log_date_format: "YYYY-MM-DD HH:mm:ss",
-      time: true
+      time: true,
+
+      kill_timeout: 5000,
+      listen_timeout: 10000
     }
   ]
 };
